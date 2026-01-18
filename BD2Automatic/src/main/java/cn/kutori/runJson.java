613d1e3a8135301cdbd93832bd656f826d321fd7
kutori <1723283961@qq.com>
2026-01-19 02:59:31 +0900

    Swing

--- BD2Automatic/src/main/java/cn/kutori/runJson.java
+++ BD2Automatic/src/main/java/cn/kutori/runJson.java
@@ -1,6 +1,5 @@
 package cn.kutori;
 
-import cn.kutori.GameUtils.Utils.ScriptRecordingTool.GameKeyRecorder;
 import cn.kutori.dto.JsonEnum;
 import cn.kutori.method.getJson;
 import lombok.extern.slf4j.Slf4j;
@@ -8,13 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class runJson {
     public static void main(String[] args) throws Exception {
-
-        GameKeyRecorder gameKeyRecorder = new GameKeyRecorder();
-        gameKeyRecorder.recordMap("RunningMap");
-
-
-
-//        getJson getJson = new getJson("RunningMap");
-//        getJson.runMap(JsonEnum.Map7_2.getPath());
+        getJson getJson = new getJson("RunningMap");
+        getJson.runMap(JsonEnum.Map7_2.getPath());
     }
 }
